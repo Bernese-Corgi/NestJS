004a9e0bcc1958f043cb673a0ec1d779a592551a
Marcella <marcella@jejememe.com>
2022-09-11 02:36:18 +0900

    주석 추가 및 사용하지 않는 파일 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
+// 데코레이터 
+// 데코레이터는 클래스에 함수 기능을 추가할 수 있다.
+// 그냥 클래스 위의 함수이고, 클래스를 위해 움직인다고 생각하면 된다.
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [MoviesController],
+  providers: [MoviesService],
 })
-export class AppModule {}
+export class AppModule {} 
+// AppModule은 비어 있는 클래스
+// 모든 것의 루트 모듈 <- 하나의 모듈에서 어플리케이션을 생성
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true }); // await NestFactory.create(AppModule)을 호출하고
+  await app.listen(3000); // 어플리케이션은 3000번의 포트를 리스닝
 }
 bootstrap();

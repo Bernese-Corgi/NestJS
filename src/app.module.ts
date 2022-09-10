30e3156c9de395609efa9e21ba248628a9acc4b1
Marcella <marcella@jejememe.com>
2022-09-11 03:53:40 +0900

    app 및 모듈 구조 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from '../app/app.controller';
 
 // 데코레이터 
 // 데코레이터는 클래스에 함수 기능을 추가할 수 있다.
 // 그냥 클래스 위의 함수이고, 클래스를 위해 움직인다고 생각하면 된다.
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {} 
 // AppModule은 비어 있는 클래스

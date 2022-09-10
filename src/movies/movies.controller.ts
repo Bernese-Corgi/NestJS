563d818a8d7d1635f922f4c61ec424d3c7392889
Marcella <marcella@jejememe.com>
2022-09-11 02:36:37 +0900

    Controller 초안 작성

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+
+@Controller('movies') // movies가 엔드포인트
+export class MoviesController {
+
+  @Get()
+  getAll() {
+    return 'This will return all movies'
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) { 
+    return `This will return one movie with the id: ${movieId}`
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after: ${searchingYear}`
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    console.log(movieData)
+    return movieData
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData
+    }
+  }
+}

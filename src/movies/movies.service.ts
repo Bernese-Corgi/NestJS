04871b3a0eb4e363b6a3234a5134aa2568b6f6b5
Marcella <marcella@jejememe.com>
2022-09-11 02:59:01 +0900

    update serviece 코드 작성

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -21,7 +21,7 @@ export class MoviesService {
 
   deleteOne(id: string) {
     this.getOne(id)
-    this.movies.filter(movie => movie.id !== +id)
+    this.movies = this.movies.filter(movie => movie.id !== +id)
   }
 
   create(movieData) {
@@ -29,6 +29,12 @@ export class MoviesService {
       id: this.movies.length + 1,
       ...movieData
     })
+  }
+
+  update(id: string, updateData) {
+    const movie = this.getOne(id)
+    this.deleteOne(id)
+    this.movies.push({ ...movie, ...updateData })
   }
 }
 
